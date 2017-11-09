55c07fe3bc07d601864144eee2e07c5582692574
leonardocardoso <leonardocardoso@leonardos-mbp.lan>
2017-11-09 13:31:55 +0000

    First commit. Beginning TDD to test and implement the prime's finder.

---
+++ test/primetables/PrimeTablesTest.java
@@ -0,0 +1,19 @@
+package primetables;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Leonardo Cardoso
+ */
+public class PrimeTablesTest {
+
+    @Test
+    public void findPrime_nEqualsOne_ReturnTwo(){
+        PrimeFinder pf = new PrimeFinder(); //Arrange
+        int result = pf.findPrime(1); //Act
+        assertEquals(result,2); //Assert
+    }
+    
+}

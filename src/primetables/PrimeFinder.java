34a9a80e9f25d9744a44ca7036bc76453d0967f4
leoncard <cardosoeb@gmail.com>
2017-11-12 03:39:03 +0000

    Added test for n=6,7 and 46, all passing. Refactored primes generator method to increase its performance by decreasing the required prime candidates to be tested and also to increase the method's readability.

--- src/primetables/PrimeFinder.java
+++ src/primetables/PrimeFinder.java
@@ -9,23 +9,24 @@ import java.util.Arrays;
 class PrimeFinder {
 
     int[] findPrimes(int n) {
-        if(numberNotAcceptedCondition(n)){
+        if(numberNotAccepted(n)){
             return defaultValue;
         }
         int [] primesArray = new int[n];
-        boolean[] numbersToCheckArray = new boolean[upperBoundary(n)];
+        int upperBoundary = upperBoundaryCalculator(n);
+        boolean[] numbersToCheckArray = new boolean[upperBoundary];
         Arrays.fill(numbersToCheckArray, true);
-        for(int primeCandidate = lowerBoundary; primeCandidate<upperBoundary(n); primeCandidate++){
+        for(int primeCandidate = lowerBoundary; primeCandidate<Math.sqrt(upperBoundary); primeCandidate++){
             if(numbersToCheckArray[primeCandidate]==true){
-                for (int nonPrime = (int)Math.pow(primeCandidate, 2); nonPrime<upperBoundary(n);nonPrime+=primeCandidate){
+                for (int nonPrime = (int)Math.pow(primeCandidate, 2); nonPrime<upperBoundary;nonPrime+=primeCandidate){
                     numbersToCheckArray[nonPrime]=false;
                 }
             }
         }
         int primesFound = 0;
-        for (int i =lowerBoundary; i<upperBoundary(n) && primesFound<n;i++){
-            if(numbersToCheckArray[i]==true){
-                primesArray[primesFound]=i;
+        for (int primeCandidate =lowerBoundary; primeCandidate<upperBoundary && primesFound<n;primeCandidate++){
+            if(numbersToCheckArray[primeCandidate]==true){
+                primesArray[primesFound]=primeCandidate;
                 primesFound++;
             }
         }
@@ -33,10 +34,12 @@ class PrimeFinder {
     }
     protected final int[] defaultValue = null;
     protected int lowerBoundary = 2;
-    protected int upperBoundary(int n){
-            return n==1 ? 3 : (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))+8); //Barkley Rosser, 1938
+    protected int upperBoundaryCalculator(int n){
+            return n==1 ? 3 : 
+                    n<=6 ? (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))+8) :
+                    (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))); //Barkley Rosser, 1938
     }
-    protected boolean numberNotAcceptedCondition(int n) {
+    protected boolean numberNotAccepted(int n) {
         return n<1;
     }
     

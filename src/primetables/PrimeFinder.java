48e320bd525c5f4f966de24e94e9bd54934890dc
leoncard <cardosoeb@gmail.com>
2017-11-12 01:07:14 +0000

    Adding failing test to check generation of the first two primes and refactoring primes generation to pass test.

--- src/primetables/PrimeFinder.java
+++ src/primetables/PrimeFinder.java
@@ -1,23 +1,30 @@
 package primetables;
 
-import java.util.ArrayList;
-import static java.util.Arrays.asList;
-import java.util.Collection;
-
 /**
  *
  * @author Leonardo Cardoso
  */
 class PrimeFinder {
 
-    int[] findPrime(int n) {
+    int[] findPrimes(int n) {
         if(numberNotAcceptedCondition(n)){
             return defaultValue;
         }
-        return new int[]{2};
+        int [] primesArray = new int[n];
+        int primesFound = 0;
+        for(int i = lowerBoundary; i<upperBoundary(n) && primesFound<n; i++){
+            if(i==2 || i%2!=0){ //initialy, just cutting off all multiples of 2, minimum solution to pass test
+                primesArray[primesFound]=i;
+                primesFound++;
+            }
+        }
+        return primesArray;
     }
     protected final int[] defaultValue = null;
-
+    protected int lowerBoundary = 2;
+    protected int upperBoundary(int n){
+            return n==1 ? 3 : (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))+8); //Barkley Rosser, 1938
+    }
     protected boolean numberNotAcceptedCondition(int n) {
         return n<1;
     }
--- test/primetables/PrimeTablesTest.java
+++ test/primetables/PrimeTablesTest.java
@@ -1,8 +1,5 @@
 package primetables;
 
-import java.util.ArrayList;
-import static java.util.Arrays.asList;
-import java.util.Collection;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
@@ -15,7 +12,7 @@ public class PrimeTablesTest {
     @Test
     public void findPrime_nEqualsOne_ReturnTwo(){
         PrimeFinder pf = new PrimeFinder(); //Arrange
-        int[] result = pf.findPrime(1); //Act
+        int[] result = pf.findPrimes(1); //Act
         int[] expected = new int[]{2};
         assertEquals(result,expected); //Assert
     }
@@ -23,9 +20,16 @@ public class PrimeTablesTest {
     @Test
     public void findPrime_nEqualsZero_ReturnNull(){
         PrimeFinder pf = new PrimeFinder(); //Arrange
-        int[] result = pf.findPrime(0); //Act
+        int[] result = pf.findPrimes(0); //Act
         int [] expected = null;
         assertEquals(result,expected); //Assert
     }
     
+    @Test
+    public void findPrime_nEqualsTwo_ReturnFirstTwoPrimes(){
+        PrimeFinder pf = new PrimeFinder(); //Arrange
+        int[] result = pf.findPrimes(2); //Act
+        int [] expected = new int[]{2,3};
+        assertEquals(result,expected); //Assert
+    }
 }

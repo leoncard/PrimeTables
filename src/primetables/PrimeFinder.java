1ad33ef2039fb1a5e66fc8bf178e62941a25138b
leoncard <cardosoeb@gmail.com>
2017-11-12 15:55:24 +0000

    Refactoring primes generator function to increase its readability and decrease its responsibilities.

--- src/primetables/PrimeFinder.java
+++ src/primetables/PrimeFinder.java
@@ -12,20 +12,34 @@ class PrimeFinder {
         if(numberNotAccepted(n)){
             return defaultValue;
         }
-        int [] primesArray = new int[n];
-        int upperBoundary = upperBoundaryCalculator(n);
+        boolean[] checkedIndexesArray = crossOutNonPrimes(n);
+        int [] primesArray = getPrimes(n, checkedIndexesArray);
+        return primesArray;
+    }
+
+        protected boolean[] crossOutNonPrimes(int n) {
+        int upperBoundary = getUpperBoundary(n);
         boolean[] numbersToCheckArray = new boolean[upperBoundary];
         Arrays.fill(numbersToCheckArray, true);
-        for(int primeCandidate = lowerBoundary; primeCandidate<Math.sqrt(upperBoundary); primeCandidate++){
-            if(numbersToCheckArray[primeCandidate]==true){
+        for(int primeCandidate = firstPrime; primeCandidate<Math.sqrt(upperBoundary); primeCandidate++){
+            if(isCandidatePrime(numbersToCheckArray, primeCandidate)){
                 for (int nonPrime = (int)Math.pow(primeCandidate, 2); nonPrime<upperBoundary;nonPrime+=primeCandidate){
                     numbersToCheckArray[nonPrime]=false;
                 }
             }
         }
-        int primesFound = 0;
-        for (int primeCandidate =lowerBoundary; primeCandidate<upperBoundary && primesFound<n;primeCandidate++){
-            if(numbersToCheckArray[primeCandidate]==true){
+        return numbersToCheckArray;
+    }
+
+    protected static boolean isCandidatePrime(boolean[] numbersToCheckArray, int primeCandidate) {
+        return numbersToCheckArray[primeCandidate]==true;
+    }
+    
+    protected int[] getPrimes(int n, boolean[] checkedNumbersArray) {
+        int [] primesArray = new int[n];
+        int primesFound=0;
+        for (int primeCandidate = firstPrimeCandidate; primesFound<n;primeCandidate++){
+            if(isCandidatePrime(checkedNumbersArray, primeCandidate)){
                 primesArray[primesFound]=primeCandidate;
                 primesFound++;
             }
@@ -33,11 +47,13 @@ class PrimeFinder {
         return primesArray;
     }
     protected final int[] defaultValue = null;
-    protected int lowerBoundary = 2;
-    protected int upperBoundaryCalculator(int n){
-            return n==1 ? 3 : 
-                    n<=6 ? (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))+8) :
-                    (int)Math.ceil(n*(Math.log(n)+Math.log(Math.log(n)))); //Barkley Rosser, 1938
+    protected final int firstPrime = 2;
+    protected int firstPrimeCandidate = firstPrime;
+    protected int getUpperBoundary(int n){
+        int numerOfPrimesToConsider = n+firstPrimeCandidate-firstPrime;
+        return numerOfPrimesToConsider==1 ? 3 : 
+                numerOfPrimesToConsider<=6 ? (int)Math.ceil(numerOfPrimesToConsider*(Math.log(numerOfPrimesToConsider)+Math.log(Math.log(numerOfPrimesToConsider)))+8) :
+                (int)Math.ceil(numerOfPrimesToConsider*(Math.log(numerOfPrimesToConsider)+Math.log(Math.log(numerOfPrimesToConsider)))); //Barkley Rosser, 1938
     }
     protected boolean numberNotAccepted(int n) {
         return n<1;

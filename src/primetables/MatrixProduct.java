d7ee44c02ea49b71d83ec0cb7e83aa495ecd8951
leoncard <cardosoeb@gmail.com>
2017-11-12 22:56:25 +0000

    Changed calculate method on MatrixProduct to pass test when input array is null.

--- src/primetables/MatrixProduct.java
+++ src/primetables/MatrixProduct.java
@@ -8,6 +8,8 @@ public class MatrixProduct implements IMatrixComputationBehaviour {
 
     @Override
     public int[][] calculate(int[] factorsArray) {
+        if(factorsArray==null){
+            return null;}
         int size = factorsArray.length;
         int [][] matrixProduct = new int[size][size];
         for(int i =0;i<size;i++){

e4c1a4edf01db359e8e7f8e69dd0da2929d9ab18
leoncard <cardosoeb@gmail.com>
2017-11-12 12:33:05 +0000

    Added tests: n=100 primes; n=minimum integer; performance for n=10Million. Refactoring tests: n=46 to n=50, data provider added to invalid number test.

--- test/primetables/PrimeTablesTest.java
+++ test/primetables/PrimeTablesTest.java
@@ -20,21 +20,34 @@ public class PrimeTablesTest {
                 { 5, new int[]{2,3,5,7,11}},
                 { 6, new int[]{2,3,5,7,11,13}},
                 { 7, new int[]{2,3,5,7,11,13,17}},
-                { 46, new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199}} 
+                { 50, new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229}},
+                {100, new int[]{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541}}
 	  };
   }
     
-    @Test
-    public void findPrime_nEqualsZero_ReturnNull(){
+  @DataProvider(name="invalidInputs")
+  public Object[][] createInvalidInputData() {
+	  return new Object[][] {
+              {Integer.MIN_VALUE,null},
+              {0,null}
+          };
+  }
+  
+    @Test(dataProvider = "invalidInputs")
+    public void findPrime_nInvalidNumber_ReturnNull(int n, int[] expected){
         PrimeFinder pf = new PrimeFinder(); //Arrange
-        int[] result = pf.findPrimes(0); //Act
-        int [] expected = null;
-        assertEquals(result,expected); //Assert
+        assertEquals(pf.findPrimes(n),expected); //Assert
     }
     
     @Test(dataProvider = "expectedPrimes")
     public void findPrime_n_ReturnExpectedPrimes(int n, int[] expected){
         PrimeFinder pf = new PrimeFinder();
         assertEquals(pf.findPrimes(n),expected);
+    }
+    
+    @Test
+    public void findPrime_n10Million_ReturnExpectedPrimes(){
+        PrimeFinder pf = new PrimeFinder();
+        assertEquals(pf.findPrimes(10000000).length,10000000);
     }
 }
